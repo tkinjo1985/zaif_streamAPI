bf8428133d9c223535e8478fe47397cee51b16f7
Kinjo.Takumi <tkinjo1@gmail.com>
2018-12-22 15:56:16 +0900

    add comment

--- main.go
+++ main.go
@@ -10,10 +10,17 @@ import (
 )
 
 // Results レスポンス用構造体
+/*
+	Asks         売り板情報
+	Lastprice    現在の終値
+	Trades       全ユーザの取引履歴
+	Bids         買い板情報
+	Currencypair 通貨ペア
+	Timestamp    タイムスタンプ
+*/
 type Results struct {
 	Asks         []interface{} `json:"asks"`
 	Lastprice    Lastprice     `json:"last_price"`
-	Targetusers  []interface{} `json:"target_users"`
 	Trades       []interface{} `json:"trades"`
 	Bids         []interface{} `json:"bids"`
 	Currencypair string        `json:"currency_pair"`
@@ -21,6 +28,10 @@ type Results struct {
 }
 
 // Lastprice Lastpriceレスポンス用構造体
+/*
+	Action ask(売り) or bid(買い)
+	Price 取引価格
+*/
 type Lastprice struct {
 	Action string  `json:"action"`
 	Price  float64 `json:"price"`
@@ -28,11 +39,14 @@ type Lastprice struct {
 
 func main() {
 
+	// 取得する通過ペア
+	currencypairs := "btc_jpy"
+
 	u := &url.URL{
 		Scheme:   "wss",
 		Host:     "ws.zaif.jp",
 		Path:     "stream",
-		RawQuery: "currency_pair=btc_jpy",
+		RawQuery: "currency_pair=" + currencypairs,
 	}
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
@@ -46,9 +60,11 @@ func main() {
 		if err := c.ReadJSON(&result); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(result.Currencypair)
 
-		time.Sleep(1 * time.Second)
+		// 必要な結果をレスポンスを指定する
+		fmt.Println(result.Lastprice)
+
+		time.Sleep(5 * time.Second)
 
 	}
 }
